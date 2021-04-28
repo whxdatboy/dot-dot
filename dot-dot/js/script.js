2609b35f0738a6a89ce6548de8b5392be18ee42c
whxdatboy <smurf1osnya@gmail.com>
2021-04-28 17:34:01 +0300

    rebuild select and options

--- dot-dot/js/script.js
+++ dot-dot/js/script.js
@@ -1,5 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-   // Custom JS
+  // Custom JS
+
+  const select = document.querySelectorAll('.section-select'),
+        option = document.querySelectorAll('.section-select__option'),
+        wrapper = document.querySelectorAll('.section-select__wrapper'),
+        selectOption = document.querySelector('.select-option');
+
+  function openOptions() {
+
+  }
+
+  console.log(select)
+
+
+  wrapper.forEach((wrap, i) => {
+    wrap.addEventListener('click', function () {
+      select[i].classList.toggle('open');
+    })
+  })
+
+  option.forEach((item, i) => {
+    item.addEventListener('click', function() {
+      let text = this.innerText;
+      selectOption.innerText = text;
+    })
+  })
 
 });
